2e6350fea1ba135132808dce31cacf361a6d3dd7
willivolt <willivolt@users.noreply.github.com>
2025-07-14 21:03:08 -0700

    Make IndicatorPattern work on Chromatik
    Chromatik seems to reset the color data to black
    after some time, so we will write the colors data
    on ever loop.

--- src/main/java/org/projectempire/lx/pattern/ui/pattern/IndicatorPattern.java
+++ src/main/java/org/projectempire/lx/pattern/ui/pattern/IndicatorPattern.java
@@ -2,21 +2,26 @@ package org.projectempire.lx.pattern.ui.pattern;
 
 import heronarts.lx.LX;
 import heronarts.lx.LXCategory;
+import heronarts.lx.LXComponent;
 import heronarts.lx.color.ColorParameter;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXModel;
 import heronarts.lx.pattern.LXPattern;
 import heronarts.lx.utils.LXUtils;
 
+import java.util.ArrayList;
+
 @LXCategory("Empire")
+@LXComponent.Name("Indicator")
 public class IndicatorPattern extends LXPattern {
-    // TODO: implements UIDeviceControls<IndicatorPattern>
     public final ColorParameter color1 = new ColorParameter("Color1").setDescription("Color of the pattern");
     public final ColorParameter color2 = new ColorParameter("Color2").setDescription("Color of the pattern");
     public final ColorParameter color3 = new ColorParameter("Color3").setDescription("Color of the pattern");
 
     private double runtime = 0;
     private boolean firstRun = true;
+    /** A list of colors for each fixture in the model. */
+    private ArrayList<ColorParameter> colorList = new ArrayList<>();
 
     public IndicatorPattern(LX lx) {
         this(lx, LXColor.RED,
@@ -35,35 +40,36 @@ public class IndicatorPattern extends LXPattern {
     }
 
     /**
-     * pick one of the 3 colors, weighted towrds the last one.
+     * pick one of the 3 colors, weighted towards the last one.
      */
-    private int pickColor() {
-        int result = 0;
+    private ColorParameter pickColor() {
+        ColorParameter result;
         int nextColor = (int) (Math.random() * 7);
         switch (nextColor) {
             case 0:
-                result = color1.getColor();
+                result = color1;
                 break;
             case 1:
             case 2:
-                result = color2.getColor();
+                result = color2;
                 break;
             default:
-                result = color3.getColor();
+                result = color3;
                 break;
         }
         return result;
     }
 
+    /**
+     * Initialize the color list with a random color for each fixture in the model.
+     */
     private void init() {
-        for (LXModel fixture : model.children) {
-            int color = pickColor();
-            for (int i = 0; i < fixture.points.length; i++) {
-                colors[fixture.points[i].index] = color;
-            }
+        colorList.ensureCapacity(model.children.length);
+        for (int i = 0; i < model.children.length; i++) {
+            colorList.add(pickColor());
         }
-
     }
+
     @Override
     protected void run(double deltaMs) {
         if (firstRun) {
@@ -71,17 +77,35 @@ public class IndicatorPattern extends LXPattern {
             firstRun = false;
         }
 
+        // Update random button colors every 5 seconds
         runtime += deltaMs;
         if (runtime >= 5000) {
             // Let's change something every 5 seconds
             runtime = 0;
             for (int i = 0; i < 5; i++) {
-                LXModel fixture = model.children[(int)LXUtils.random(0, model.children.length)];
-                int color = pickColor();
+                // Pick a random fixture and set its color
+                int fixtureIndex = (int) LXUtils.random(0, model.children.length);
+                if (fixtureIndex < colorList.size()) {
+                    colorList.set(fixtureIndex, pickColor());
+                }
+            }
+        }
+
+        // Chromatik seems to want all pixes set on every call to run()
+        for (int i = 0; i < model.children.length; i++) {
+            if (i < colorList.size()) { // always true?
+                LXModel fixture = model.children[i];
+                int color = colorList.get(i).getColor();
                 for (int index = 0; index < fixture.points.length; index++) {
                     colors[fixture.points[index].index] = color;
                 }
             }
         }
+    }
+
+    @Override
+    protected void onModelChanged(LXModel model) {
+        super.onModelChanged(model);
+        init();
     }
 }
